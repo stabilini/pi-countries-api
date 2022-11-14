1de2f40f90c17c6a0de553e65c300a731699d425
Nicolas Stabilini <stabilini@gmail.com>
2022-11-13 23:45:44 -0300

    prueba

--- src/controllers/getActivities.js
+++ src/controllers/getActivities.js
@@ -2,13 +2,12 @@ const { Country, Activity } = require("../db.js");
 
 const getActivities = async (req, res) => {
   try {
-    let result = await Country.findAll({include: Activity});
-    let act = {};
+    let result = await Activity.findAll();
+    let act = {}
+
     for (let i = 0; i < result.length; i++) {
-      for (let x = 0; x < result[i].activities.length; x++) {
-        let key = result[i].activities[x].name;
-        act[key] = true;
-      } 
+      let key = result[i].name;
+      act[key] = true;
     }
     res.status(200).json(act);
   } catch (error) {
