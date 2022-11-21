04221161667dd01d905440e415d3393fb9daff4d
Nicolas Stabilini <stabilini@gmail.com>
2022-11-21 11:24:43 -0300

    test

--- src/db.js
+++ src/db.js
@@ -10,11 +10,11 @@ const path = require('path');
 const sequelize = new Sequelize('postgresql://postgres:WNfvwkUwFaZIWRsyNyci@containers-us-west-127.railway.app:5941/railway', {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true
-    }
-  }
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true
+  //   }
+  // }
 });
 const basename = path.basename(__filename);
 
